a0eae6de8381bb7511fc69dcf3e5f660ef7ff011
cristo <cristoangulo11@gmail.com>
2019-10-07 20:09:59 -0700

    Tarea 3.1
    De acuerdo a los ejemplos vistos en clase y la lectura. Implementa en c# la clase Pelicula con los siguientes atributos publicos:
    
    Pelicula
    atributo	tipo
    titulo	String
    año	Int16
    país	String
    director	String
    1.Como primer paso crea solo la clase con los atributos públicos e inicializalos en Program.Main() directamente.
    2.Crea dos objetos tipo Pelicula con dos peliculas ganadoras de un Oscar.
    3.Imprime en la consola el titulo y año de las peliculas. Sube la primera versión a GitHub.

---
+++ Programa_Pelicula/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Programa_Pelicula
+{
+    class Pelicula
+    {
+        public string titulo;
+        public int anio;
+        public string pais;
+        public string director;
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pelicula p1 = new Pelicula();
+            p1.titulo = "Black Phanter";
+            p1.anio = 2018;
+            p1.pais = "USA";
+            p1.director = "Ryan Coogler";
+            Console.WriteLine("titulo: {0}\nAnio: {1}\npais: {2}\ndirector: {3}\n", p1.titulo, p1.anio, p1.pais, p1.director);
+        }
+    }
+}
