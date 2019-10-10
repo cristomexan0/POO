c687b10d8c8ec8f0a1f75df35415edcb4e7b80ee
cristo <cristoangulo11@gmail.com>
2019-10-09 21:03:35 -0700

    Tarea 3.2
    Ahora cambia los atributos a private. Agrega los métodos necesarios para crear los objetos de la siguiente manera:
    utilizando el metodo getset

--- Programa_Pelicula/Program.cs
+++ Programa_Pelicula/Program.cs
@@ -6,21 +6,39 @@ namespace Programa_Pelicula
     {
         private string titulo;
         private int anio;
-        private string pais;
-        private string director;
+    
+     public void Settitulo( string titulo )
+     {
+        this.titulo = titulo;
+     }
+     public void Gettitulo( )
+     {
+        return this.titulo;
+     }
+     public void Setanio( int anio )
+     {
+        this.anio = anio;
+     } 
+     public void Getanio( )
+     {
+        return this.anio;
+     }
     }
-
 
     class Program
     {
         static void Main(string[] args)
         {
             Pelicula p1 = new Pelicula();
-            p1.titulo = "Black Phanter";
-            p1.anio = 2018;
-            p1.pais = "USA";
-            p1.director = "Ryan Coogler";
-            Console.WriteLine("titulo: {0}\nAnio: {1}\n", p1.titulo, p1.anio);
+            p1. Settitulo ( "Black Phanter" ) ;
+            p1.Setanio  ( 2018 ) ;
+            Console.WriteLine("titulo: {0}\n Anio: {1}\n", p1.Gettitulo(), p1.Getanio());
+
+            Pelicula p2 = new Pelicula();
+            p2.Settitulo  (" MAD MAX " ) ;
+            p2.Setanio  ( 1979 ) ;
+            Console.WriteLine("titulo: {0}\n Anio: {1}\n", p2.Gettitulo(), p2.Getanio());
+
         }
     }
 }
