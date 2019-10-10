373b0ffb4f4b51de3b86e8106a7dece3d9019a24
cristo <cristoangulo11@gmail.com>
2019-10-10 14:29:14 -0700

    Traea 3.3

--- Programa_Pelicula/Program.cs
+++ Programa_Pelicula/Program.cs
@@ -23,8 +23,23 @@ namespace Programa_Pelicula
      {
         return this.anio;
      }
-    }
+   
+      public Pelicula ()
+      {
 
+      }
+            public Pelicula (string titulo, int anio)
+      {
+         this.titulo = titulo;
+         this.anio = anio;
+      }
+      
+      public void imprime()
+      {
+       Console.WriteLine("titulo: {0}\n Anio: {1}\n", this.titulo, this.anio);
+      //Console.WriteLine(this.titulo, this.anio);
+      }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -32,12 +47,16 @@ namespace Programa_Pelicula
             Pelicula p1 = new Pelicula();
             p1. Settitulo ( "Black Phanter" ) ;
             p1.Setanio  ( 2018 ) ;
-            Console.WriteLine("titulo: {0}\n Anio: {1}\n", p1.Gettitulo(), p1.Getanio());
+           // Console.WriteLine("titulo: {0}\n Anio: {1}\n", p1.Gettitulo(), p1.Getanio());
+
+            p1.imprime ();
 
             Pelicula p2 = new Pelicula();
             p2.Settitulo  (" MAD MAX " ) ;
             p2.Setanio  ( 1979 ) ;
-            Console.WriteLine("titulo: {0}\n Anio: {1}\n", p2.Gettitulo(), p2.Getanio());
+           // Console.WriteLine("titulo: {0}\n Anio: {1}\n", p2.Gettitulo(), p2.Getanio());
+
+            p2.imprime();
 
         }
     }
