18c7643b5b8a2b2c82df2d51f21df527466a59f0
cristo <cristoangulo11@gmail.com>
2019-10-10 18:48:09 -0700

    Duracion 1.1

---
+++ Duracion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Duracion
+{
+    class Duracion
+    {
+        private int Horas;
+        private int Minutos;
+        private int Segundos;
+
+        public Duracion(int Horas, int Minutos, int Segundos)
+        {
+            this.Horas = Horas;
+            this.Minutos = Minutos;
+            this.Segundos = Segundos;
+        }
+        public void imprime()
+        {
+            Console.WriteLine("Horas: {0}\nMinutos: {1}\nSegundos: {2}", this.Horas, this.Minutos, this.Segundos);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duracion D = new Duracion(2,30,0);
+            D.imprime();
+        }
+    }
+}
