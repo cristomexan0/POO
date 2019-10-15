82407b5b3f982c8dcfb33817ba6d59e00332972a
cristo <cristoangulo11@gmail.com>
2019-10-15 00:11:33 -0700

    Musico
    utilizamos lo que es "protected" que es como un private que si pude eredar, utilizamos un constructorllamado bajista.

---
+++ Musico/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Musico
+{
+    class Musico
+    {
+        protected string nombre;
+
+        public Musico(string n)
+        {
+            nombre = n;
+        }
+        public void Saluda()
+        {
+            Console.WriteLine("Hola, soy {0}", nombre);
+        }
+        public void Afina()
+        {
+            Console.WriteLine("{0}, afina su instrumento", nombre);
+        }
+
+    }
+    class Bajista:Musico
+    {
+        private string bajo;
+        public Bajista (string no, string bajo):base(no)
+        {
+            this.bajo = bajo;
+        }
+        public void Afina()
+        {
+            Console.WriteLine("{0}, afina su bajo {1}", nombre, bajo);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musico tom = new Musico("Tom");
+            Bajista flea = new Bajista("Flea", "Gibson");
+            tom.Saluda();
+            flea.Saluda();
+            tom.Afina();
+            flea.Afina();
+        }
+    }
+}
