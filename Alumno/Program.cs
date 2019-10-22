4585e8d19f8d2ccac58ee30055d0d52c31f32dc2
cristo <cristoangulo11@gmail.com>
2019-10-22 12:46:42 -0700

    Alumno1.2Lista

--- Alumno/Program.cs
+++ Alumno/Program.cs
@@ -53,13 +53,39 @@ namespace Alumno
     {
         static void Main(string[] args)
         {
-            Alumno a1 = new Alumno("Nombre del alumno: Pepe\nApellido: Zanches\nNumero de control: 245764\nCorreo electronico: jolo@gmail.com");
-            //N_control b = new N_control("{1}122554");
+            Alumno a1 = new Alumno("Nombre del alumno: Pepe\nApellido: Zanches\nNumero de control: 245764\nCorreo electronico: PpZnchs@gmail.com\n\n");
+            //N_control b = new N_control("{1}245764");
            // Console.WriteLine(p.Nombre);  
            a1.print();
+
+           Alumno a2 = new Alumno("Nombre del alumno: Poncho\nApellido: Tunas\nNumero de control: 365463\nCorreo electronico: PonchitoTun027@gmail.com\n\n");
+            //N_control b = new N_control("{2}365463");
+           // Console.WriteLine(p.Nombre);  
+           a2.print();
+
+           Alumno a3 = new Alumno("Nombre del alumno: Julio\nApellido: Zapata\nNumero de control: 876469\nCorreo electronico: JulionZapaton105@gmail.com\n");
+            //N_control b = new N_control("{3}876469");
+           // Console.WriteLine(p.Nombre);  
+           a3.print();
            /*b.print();
            //List<Alumno> grupo = new List<Alumno>();
            grupo.Add(a1);
+           foreach(Alumno m in grupo)
+            {
+                m.Alumno;
+            }*/
+
+           /*b.print();
+           //List<Alumno> grupo = new List<Alumno>();
+           grupo.Add(a2);
+           foreach(Alumno m in grupo)
+            {
+                m.Alumno;
+            }*/
+
+           /*b.print();
+           //List<Alumno> grupo = new List<Alumno>();
+           grupo.Add(a3);
            foreach(Alumno m in grupo)
             {
                 m.Alumno;
