cc4d0cfe9283dadc285edffb7073409b3b64190f
cristo <cristoangulo11@gmail.com>
2019-11-03 17:37:27 -0800

    Figura

---
+++ Rectangulo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rectangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
