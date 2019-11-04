7e02f1a02aa87dffa7d075937f7e14bbd3b97a53
cristo <cristoangulo11@gmail.com>
2019-11-03 17:59:16 -0800

    Fraction(Ejercicio de Examen)

--- Fraction/Program.cs
+++ Fraction/Program.cs
@@ -2,11 +2,37 @@
 
 namespace Fraction
 {
+    class Fraction 
+    {
+        private readonly int num;
+        private readonly int den;
+        public Fraction(int numerator, int denominator)
+        {
+            num = numerator;
+            den = denominator;
+
+            if(denominator == 0)
+            {
+                from new ArgumentException("No creo");
+            }
+
+        }
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            return new Fraction(a.num * b.den + a.den * b.num, a.den * b.den);
+        }
+        public static Fraction operator -(Fraction a)
+        {
+            retur new Fraction(-a.num, a,den);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Fraction a = new Fraction(5,4);
+            var b = new Fraction(1,2);
+            Console.WriteLine(a+b);
         }
     }
 }
